1d2adc0404dbb246ccac1ee4d3626145652116fd
JamesCroissant <yuham1014@gmail.com>
2024-03-03 14:25:35 -0800

    add user model

---
+++ server/models/userModel.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+type User = {
+  _id: string;
+  username: string;
+  email: string;
+  password: string;
+  profilePicture: string;
+  isAdmin: boolean;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model<User>('User', userSchema);
+
+export default User;
