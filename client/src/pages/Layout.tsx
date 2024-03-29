fd302522d5a2d73f2def12b18e4eae5b741615bc
JamesCroissant <yuham1014@gmail.com>
2024-03-29 15:49:35 -0700

    add: create main pages

---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "@/components/layout/Footer"
+import { Header } from "@/components/layout/Header"
+import { Outlet } from "react-router"
+
+export const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
