6d20ef618cb0b333fa9d18ca9bf73a835aabd896
MarinaYano <myano2621@gmail.com>
2024-04-05 15:59:41 -0700

    add get method

--- server/routes/productRoute.ts
+++ server/routes/productRoute.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createProduct } from "../controllers/productController";
+import { createProduct, getProduct } from "../controllers/productController";
 
 const router = express.Router();
 
 router.post('/post', createProduct);
+router.get('/:id', getProduct)
 
 export default router;
