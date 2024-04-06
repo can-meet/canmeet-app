77e68aa980bc659cac9979e337dc70fe2c662fed
JamesCroissant <yuham1014@gmail.com>
2024-04-06 16:44:21 -0700

    add: clip-path styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -71,7 +71,21 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      clipPath: {
+        'diagonal': 'polygon(100% 0, 0% 100%, 100% 100%)',
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('@tailwindcss/custom-forms'),
+    function({ addUtilities, theme }) {
+      const newUtilities = {
+        '.clip-diagonal': {
+          clipPath: theme('clipPath.diagonal'),
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    }
+  ],
 }
