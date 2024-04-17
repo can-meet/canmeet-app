4e36681c41a893c08fed59befcb00dcc0ef24fbe
JamesCroissant <yuham1014@gmail.com>
2024-04-16 22:41:39 -0700

    fix: change some codes

---
+++ server/server.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/authRoute';
+import productRoutes from './routes/productRoute';
+import commentRoutes from './routes/commentRoute';
+import replyRoutes from './routes/replyRoute';
+
+
+dotenv.config();
+
+const app: express.Express = express();
+const mongoURI = process.env.MONGODB_URI as string;
+const port = process.env.PORT;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/replies', replyRoutes);
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
