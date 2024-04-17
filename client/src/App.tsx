1f7fd64036ea1f714e9bb329697916c5cb21c0bb
JamesCroissant <yuham1014@gmail.com>
2024-04-16 22:38:01 -0700

    fix: change some codes

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Layout } from './pages/Layout'
+import { Home } from './pages/Home'
+import { SignUp } from './pages/SignUp'
+import { Login } from './pages/Login'
+import { NotFound } from './pages/NotFound'
+import DetailProduct from './pages/product/DetailProduct'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="product/:pid" element={<DetailProduct />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
