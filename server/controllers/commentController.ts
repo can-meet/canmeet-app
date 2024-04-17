ff25cec2ea37c28ecea544271e986465bede2c7f
JamesCroissant <yuham1014@gmail.com>
2024-04-16 22:38:42 -0700

    fix: create each controllers

---
+++ server/controllers/commentController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import Comment from "../models/commentModel";
+import User from "../models/userModel";
+import Product from "../models/productModel";
+import mongoose from 'mongoose';
+
+export const createComment = async (req: Request, res: Response) => {
+  try {
+    const { text, userId, productId } = req.body;
+    const user = await User.findById(userId);
+    const product = await Product.findById(productId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    const comment = new Comment({ 
+      text, 
+      user: user._id, 
+      product: product._id 
+    });
+
+    await comment.save();
+
+    product.comments.push(comment);
+    await product.save();
+
+    res.status(200).json(comment);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+}
+
+export const getComments = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
+
+    const product = await Product.findById(productId).populate({
+      path: 'comments',
+      populate: [
+        {
+          path: 'user',
+          select: 'username profilePicture',
+          model: 'User',
+        },
+      ],
+    });
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.status(200).json(product.comments);
+
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+}
---
+++ server/controllers/productController.ts
@@ -0,0 +1,80 @@
+import { Request, Response } from "express";
+import Product from "../models/productModel";
+import User from "../models/userModel";
+
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const {
+      userId,
+      product_name,
+      price,
+      image,
+      product_status,
+      description,
+      payment_method,
+      location,
+      sale_status
+    } = req.body;
+
+    if(!product_name || !price || !image || !product_status || !description || !payment_method || !location || !sale_status) {
+      return res.status(400).json({ message: 'Still some blanks left' })
+    }
+
+    const userExists = await User.findById(userId)
+    if(!userExists) {
+      return res.status(404).json({ message: 'User not found' }) 
+    }
+  
+    const maxLength = 500;
+    if (description.length > maxLength) {
+      return res.status(400).json({ error: `Text must be less than ${maxLength} characters` });
+    }
+  
+    const newProduct = new Product({
+      user: userId,
+      product_name,
+      price,
+      image,
+      product_status,
+      description,
+      payment_method,
+      location,
+      sale_status
+    })
+  
+    await newProduct.save()
+    res.status(201).json(newProduct)
+  } catch (error) {
+    console.log("Error in product controller")
+    res.status(500).json({ error: (error as Error).message })
+  }
+}
+
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+		if (!product) {
+			return res.status(404).json({ error: "product not found" });
+		}
+
+		res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+}
+
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find({}).sort({ createdAt: -1 });
+
+		if (!products) {
+			return res.status(404).json({ error: "product not found" });
+		}
+
+		res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
+}
