228f71e31dcd6dc08d1c860a987bfadf8d137f19
JamesCroissant <yuham1014@gmail.com>
2024-04-16 22:36:19 -0700

    fix: create each schema

---
+++ client/src/schema/comment.ts
@@ -0,0 +1,16 @@
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+
+export const commentSchema = z.object({
+  userId: z
+    .string(),
+  productId: z
+    .string(),
+  text: z
+    .string()
+    .max(200, { message: 'コメントは100文字以内で記載してください。' })
+});
+
+export type CommentSchema = z.infer<typeof commentSchema>;
+export const commentResolver = zodResolver(commentSchema);
---
+++ client/src/schema/reply.ts
@@ -0,0 +1,16 @@
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+
+export const replySchema = z.object({
+  userId: z
+    .string(),
+  commentId: z
+    .string(),
+  text: z
+    .string()
+    .max(200, { message: 'コメントは100文字以内で記載してください。' })
+});
+
+export type ReplySchema = z.infer<typeof replySchema>;
+export const replyResolver = zodResolver(replySchema);
