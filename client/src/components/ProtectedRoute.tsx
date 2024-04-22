80b038c94f36a6bb7e924c6d7e348c77e07915b5
JamesCroissant <yuham1014@gmail.com>
2024-04-21 20:27:05 -0700

    fix: set the routing of profile page

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,8 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { Outlet, Navigate } from 'react-router-dom';
+
+export const ProtectedRoute = () => {
+  const { currentUser } = useSelector((state: RootState) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to='/login' />;
+}
