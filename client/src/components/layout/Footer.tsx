0a71f088a736e661e6c4f0cd329aab431ab95ce0
JamesCroissant <yuham1014@gmail.com>
2024-04-21 20:26:19 -0700

    fix: change the icon links of footer

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,19 +1,26 @@
-import { Link } from "react-router-dom";
-import { PiPaperPlaneRight } from "react-icons/pi";
+import { Link, useLocation } from "react-router-dom";
+import { PiPaperPlaneRight, PiPaperPlaneRightFill } from "react-icons/pi";
 import { IoMdHome } from "react-icons/io";
 import { FaCamera, FaUser } from "react-icons/fa";
+import { FiCamera, FiUser } from "react-icons/fi";
+import { VscHome } from "react-icons/vsc";
 
 export const Footer = () => {
+  const location = useLocation();
   return (
     <footer className="fixed bottom-0 w-full bg-white z-10">
       <div className="flex justify-around items-center p-3 border-t-[1px]">
         <Link to="/">
-          <IoMdHome className="text-2xl" />
+          {location.pathname === '/' ? <IoMdHome className="text-2xl" /> : <VscHome className="text-2xl" />}
         </Link>
-        <FaCamera className="text-xl" />
-        <PiPaperPlaneRight className="text-xl" />
+        <Link to="/">
+          {location.pathname === '/' ? <FaCamera className="text-xl border-black" /> : <FiCamera className="text-xl" />}
+        </Link>
+        <Link to="/">
+          {location.pathname === '/' ? <PiPaperPlaneRightFill className="text-xl" /> : <PiPaperPlaneRight className="text-xl" />}
+        </Link>
         <Link to="/profile">
-          <FaUser className="text-lg" />
+          {location.pathname === '/profile' ? <FaUser className="text-lg" /> : <FiUser className="text-2xl" />}
         </Link>
       </div>
     </footer>
