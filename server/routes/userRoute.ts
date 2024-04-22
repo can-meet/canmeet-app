6ca6f058efeaffca783fc034732d4374542cc400
JamesCroissant <yuham1014@gmail.com>
2024-04-21 20:22:02 -0700

    fix: change user routing

---
+++ server/routes/userRoute.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { getUser } from "../controllers/userController";
+
+const router = express.Router();
+
+router.get('/:userId', getUser);
+
+
+export default router;
