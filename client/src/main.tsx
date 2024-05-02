5368c4edaed6ccf40bd6c2d0d16b3e2263871446
JamesCroissant <yuham1014@gmail.com>
2024-05-01 19:58:20 -0700

    fix: set react query provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,14 +5,20 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.tsx";
 import "./index.css";
 import { persistor, store } from "./redux/store";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+
+const queryClient = new QueryClient();
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<App />
-			</PersistGate>
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<App />
+				</PersistGate>
+			</Provider>
+		</QueryClientProvider>
 	</React.StrictMode>,
 );
