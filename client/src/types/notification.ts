53fab29374d3f225f762ccbb6f7718558ba2d937
JamesCroissant <yuham1014@gmail.com>
2024-05-07 16:17:47 -0700

    add: set notification type and global state

---
+++ client/src/types/notification.ts
@@ -0,0 +1,12 @@
+import { Product } from "./product";
+import { User } from "./user";
+
+export type Notification = {
+	_id: string;
+  receiver: User;   // 通知を受け取るユーザー
+	sender: User;   // 通知を送るユーザー
+  type: string;  // 通知の種類（purchase, message, comment)
+  content: string;  // 通知の内容
+	isRead: boolean;  // 未読か既読か
+  product: Product; // 関連する商品
+};
