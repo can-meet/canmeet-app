716d2a224756389f20e0e71735101a7996e311ae
MarinaYano <myano2621@gmail.com>
2024-05-29 22:08:28 -0700

    change url on socket.ts

--- server/socket/socket.ts
+++ server/socket/socket.ts
@@ -9,8 +9,8 @@ const app: express.Express = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    // origin: 'https://canmeet.netlify.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://canmeet.netlify.app',
+    // origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
 });
