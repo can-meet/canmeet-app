9f931a57615da402650329fc3db23c24e3c52b89
JamesCroissant <yuham1014@gmail.com>
2024-07-03 01:17:38 -0700

    fix: add default image in reply card

--- client/src/components/product/reply/ReplyCard.tsx
+++ client/src/components/product/reply/ReplyCard.tsx
@@ -10,7 +10,13 @@ export const ReplyCard = ({ reply }: ReplyCardProps) => {
     <div className='ml-6'>
       <div className='flex items-center gap-2'>
         <Avatar>
-          <AvatarImage src={reply.user.profilePicture} />
+          <AvatarImage
+            src={
+              reply.user.profilePicture ||
+              'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+            }
+            className='h-10'
+          />
         </Avatar>
         <p>{reply.user.username}</p>
       </div>
